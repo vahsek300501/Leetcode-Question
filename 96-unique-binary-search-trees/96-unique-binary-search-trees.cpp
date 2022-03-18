15e2a635eb708fb4632af30095e1d30d590f49e0
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-03-19 00:01:38 +0530

    Time: 4 ms (22.84%), Space: 5.9 MB (67.00%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int numTreesUtil(int s, int e) {
+    int numTreesUtilRecursion(int s, int e) {
         if (s > e)
             return 0;
         if (s == e)
@@ -8,8 +8,8 @@ public:
 
         int tmpAns = 0;
         for (int i = s; i <= e; i++) {
-            int leftPossibleSubtrees = numTreesUtil(s, i - 1);
-            int rightPossibleSubtrees = numTreesUtil(i + 1, e);
+            int leftPossibleSubtrees = numTreesUtilRecursion(s, i - 1);
+            int rightPossibleSubtrees = numTreesUtilRecursion(i + 1, e);
             if (leftPossibleSubtrees == 0 && rightPossibleSubtrees == 0)
                 continue;
             else if (leftPossibleSubtrees == 0 && rightPossibleSubtrees != 0)
@@ -22,11 +22,46 @@ public:
         return tmpAns;
     }
 
+    int numTreesUtilMemoization(int s, int e, int **dp) {
+        if (s > e)
+            return 0;
+        if (s == e)
+            return 1;
+        if (dp[s][e] != -1)
+            return dp[s][e];
+
+        int tmpAns = 0;
+        for (int i = s; i <= e; i++) {
+            int leftPossibleSubtrees = numTreesUtilMemoization(s, i - 1, dp);
+            int rightPossibleSubtrees = numTreesUtilMemoization(i + 1, e, dp);
+            if (leftPossibleSubtrees == 0 && rightPossibleSubtrees == 0)
+                continue;
+            else if (leftPossibleSubtrees == 0 && rightPossibleSubtrees != 0)
+                tmpAns += rightPossibleSubtrees;
+            else if (leftPossibleSubtrees != 0 && rightPossibleSubtrees == 0)
+                tmpAns += leftPossibleSubtrees;
+            else
+                tmpAns += (leftPossibleSubtrees * rightPossibleSubtrees);
+        }
+        dp[s][e] = tmpAns;
+        return tmpAns;
+    }
+
     int numTrees(int n) {
-        if(n == 18)
-            return 477638700;
-        if(n == 19)
-            return 1767263190;
-        return numTreesUtil(1, n);
+        int **dp = new int *[n + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i] = new int[n + 1];
+        }
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        int uniqueBSTs = numTreesUtilMemoization(1, n, dp);
+        for (int i = 0; i <= n; i++) {
+            delete[] dp[i];
+        }
+        delete[] dp;
+        return uniqueBSTs;
     }
 };
