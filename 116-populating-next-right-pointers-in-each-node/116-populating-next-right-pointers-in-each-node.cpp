174fa3f39717dd4cd4f5ba977dfb470879139b49
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-03-19 02:40:40 +0530

    Time: 23 ms (68.45%), Space: 17.4 MB (12.97%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node *connect(Node *root) {
+        if(root == nullptr)
+            return nullptr;
+        queue<Node *> traversalQueue;
+        traversalQueue.push(root);
+        Node *endingNode = new Node(INT_MIN);
+        traversalQueue.push(endingNode);
+
+        while (true) {
+            if (traversalQueue.size() == 1 && traversalQueue.front()->val == INT_MIN)
+                break;
+            Node *frontNode = traversalQueue.front();
+            traversalQueue.pop();
+            if (frontNode->val == INT_MIN) {
+                traversalQueue.push(frontNode);
+                continue;
+            }
+            if (traversalQueue.front()->val == INT_MIN) {
+                frontNode->next = nullptr;
+            } else {
+                frontNode->next = traversalQueue.front();
+            }
+            if (frontNode->left != nullptr)
+                traversalQueue.push(frontNode->left);
+            if (frontNode->right != nullptr)
+                traversalQueue.push(frontNode->right);
+        }
+        return root;
+    }
+};
