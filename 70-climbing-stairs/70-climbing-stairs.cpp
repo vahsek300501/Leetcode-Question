02541c489d4d4353af4c6aea19af18f8a03cbed2
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-03-24 18:27:50 +0530

    Time: 6 ms (10.41%), Space: 5.8 MB (82.89%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,43 +1,16 @@
 class Solution {
 public:
-    int climbStairsMemoization(int n, int *dp) {
-        if (n < 0)
-            return 0;
-        if (n == 0)
-            return 1;
-        if (dp[n] != -1)
-            return dp[n];
-        int singleJump = climbStairsMemoization(n - 1, dp);
-        int doubleJump = climbStairsMemoization(n - 2, dp);
-        dp[n] = singleJump + doubleJump;
-        return singleJump + doubleJump;
-    }
-
-    int climbStairsMemoizationUtil(int n) {
-        int *dp = new int[n + 1];
-        for(int i=0;i<=n;i++) {
-            dp[i] = -1;
-        }
-        return climbStairsMemoization(n, dp);
-    }
-    int climbStairsDP(int n) {
-        int *dp = new int[n + 1];
-        for (int i = 0; i <= n; i++) {
-            dp[i] = -1;
-        }
-        dp[0] = 1;
+    int climbStairsDPSpaceOptimized(int n) {
+        int singleStepPrev = 1;
+        int doubleStepPrev = 0;
         for (int i = 1; i <= n; i++) {
-            int singleStep = dp[i - 1];
-            int doubleStep = 0;
-            if (i - 2 >= 0)
-                doubleStep = dp[i - 2];
-            dp[i] = singleStep + doubleStep;
+            int tmpAns = singleStepPrev + doubleStepPrev;
+            doubleStepPrev = singleStepPrev;
+            singleStepPrev = tmpAns;
         }
-        int ans = dp[n];
-        delete[] dp;
-        return ans;
+        return singleStepPrev;
     }
     int climbStairs(int n) {
-        return climbStairsDP(n);   
+        return climbStairsDPSpaceOptimized(n);   
     }
 };
