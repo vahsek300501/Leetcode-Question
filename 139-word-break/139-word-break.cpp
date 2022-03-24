b2d14d51d683c46bd138f647a39766bfab4e69aa
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-03-24 22:47:57 +0530

    Time: 11 ms (64.45%), Space: 9.4 MB (69.14%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    bool wordBreakMemoization(string s, int cntIndex, unordered_map<string, bool> umap, int *dp) {
+    bool wordBreakMemoization(string s, unordered_map<string, bool> &dictionary, int cntIndex, vector<int> &dp) {
         if (cntIndex >= s.length())
             return true;
-        if (dp[cntIndex] == 1)
+        if (dp.at(cntIndex) == 1)
             return true;
-        if (dp[cntIndex] == 0)
+        if (dp.at(cntIndex) == 0)
             return false;
-
-        string tmpStr;
+        string tmpWord;
         for (int i = cntIndex; i < s.length(); i++) {
-            tmpStr += s.at(i);
-            if (umap.find(tmpStr) != umap.end()) {
-                bool found = wordBreakMemoization(s, i + 1, umap, dp);
+            tmpWord += s.at(i);
+            if (dictionary.find(tmpWord) != dictionary.end()) {
+                bool found = wordBreakMemoization(s, dictionary, i + 1, dp);
                 if (found) {
                     dp[cntIndex] = 1;
                     return true;
@@ -23,14 +22,19 @@ public:
         return false;
     }
 
-    bool wordBreak(const string &s, vector<string> &wordDict) {
-        unordered_map<string, bool> dictionary;
-        for (int i = 0; i < wordDict.size(); i++) {
-            dictionary[wordDict.at(i)] = true;
+    bool wordBreakMemoizationUtil(string s, unordered_map<string, bool> &dictionary) {
+        vector<int> dp;
+        for (int i = 0; i < s.length(); i++) {
+            dp.push_back(-1);
         }
-        int dp[s.length() + 1];
-        for (int i = 0; i <= s.length(); i++)
-            dp[i] = -1;
-        return wordBreakMemoization(s, 0, dictionary, dp);
+        return wordBreakMemoization(s, dictionary, 0, dp);
+    }
+
+    bool wordBreak(string s, vector<string> &wordDict) {
+        unordered_map<string, bool> dict;
+        for (auto &i : wordDict) {
+            dict[i] = true;
+        }
+        return wordBreakMemoizationUtil(s, dict);
     }
 };
