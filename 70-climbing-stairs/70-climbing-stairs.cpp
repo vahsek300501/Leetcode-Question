f5935f9a1b432bf6fcfa2d6ce13ca3bdb6cb0515
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-03-24 18:19:29 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (29.69%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,42 +1,26 @@
 class Solution {
 public:
-    int climbStairsRecursive(int n) {
-        if (n == 0 || n == 1)
-            return 1;
-        return climbStairsRecursive(n - 1) + climbStairsRecursive(n - 2);
-    }
-
     int climbStairsMemoization(int n, int *dp) {
-        if(n < 0)
+        if (n < 0)
             return 0;
-        if (n == 0 || n == 1)
+        if (n == 0)
             return 1;
         if (dp[n] != -1)
             return dp[n];
-        int tmp = climbStairsRecursive(n - 1) + climbStairsRecursive(n - 2);
-        dp[n] = tmp;
-        return tmp;
+        int singleJump = climbStairsMemoization(n - 1, dp);
+        int doubleJump = climbStairsMemoization(n - 2, dp);
+        dp[n] = singleJump + doubleJump;
+        return singleJump + doubleJump;
     }
 
     int climbStairsMemoizationUtil(int n) {
-        int dp[n + 1];
-        for (int i = 0; i <= n; i++) {
+        int *dp = new int[n + 1];
+        for(int i=0;i<=n;i++) {
             dp[i] = -1;
         }
         return climbStairsMemoization(n, dp);
     }
-
-    int climbingStairsDynamicProgramming(int n) {
-        int dp[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        return dp[n];
-    }
-
     int climbStairs(int n) {
-        return climbingStairsDynamicProgramming(n);
+        return climbStairsMemoizationUtil(n);   
     }
 };
