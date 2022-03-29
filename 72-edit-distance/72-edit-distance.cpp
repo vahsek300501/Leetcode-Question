8185be30dd23e997172d2ece3c92882f8e47fac0
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-03-29 18:36:13 +0530

    Time: 7 ms (92.29%), Space: 8.6 MB (63.99%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,47 +1,30 @@
 class Solution {
 public:
-    int minDistanceMemoization(string word1, string word2, int **dp) {
-        if (word1.empty() && !word2.empty())
-            return word2.length();
-        if (word2.empty() && !word1.empty())
-            return word1.length();
-        if (word1.empty() && word2.empty())
-            return 0;
-        if (dp[word1.length()][word2.length()] != -1) {
-            return dp[word1.length()][word2.length()];
-        }
-
-        if (word1.at(0) == word2.at(0)) {
-            int ans = minDistanceMemoization(word1.substr(1), word2.substr(1),dp);
-            dp[word1.length()][word2.length()] = ans;
-            return ans;
-        }
-
-        int addCharacter = 1 + minDistanceMemoization(word1, word2.substr(1), dp);
-        int deleteCharacter = 1 + minDistanceMemoization(word1.substr(1), word2, dp);
-        int replaceCharacter = 1 + minDistanceMemoization(word1.substr(1), word2.substr(1), dp);
-        dp[word1.length()][word2.length()] = min(addCharacter, min(deleteCharacter, replaceCharacter));
-        return min(addCharacter, min(deleteCharacter, replaceCharacter));
-    }
-
-    int minDistanceMemoizationUtil(string word1, string word2) {
+    int minDistanceDynamicProgramming(string word1, string word2) {
         int **dp = new int *[word1.length() + 1];
         for (int i = 0; i <= word1.length(); i++) {
             dp[i] = new int[word2.length() + 1];
         }
+
         for (int i = 0; i <= word1.length(); i++) {
-            for (int j = 0; j <= word2.length(); j++) {
-                dp[i][j] = -1;
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= word2.length(); i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min(dp[i - 1][j], min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+                }
             }
         }
-        int ans = minDistanceMemoization(word1, word2, dp);
-        for (int i = 0; i <= word1.length(); i++) {
-            delete[] dp[i];
-        }
-        delete[] dp;
-        return ans;
+        return dp[word1.length()][word2.length()];
     }
     int minDistance(string word1, string word2) {
-        return minDistanceMemoizationUtil(word1,word2);
+        return minDistanceDynamicProgramming(word1,word2);   
     }
 };
