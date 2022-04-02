95debcabf13fdb743f2dd9e1305cbf36ef74cb99
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-04-02 18:50:14 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (50.14%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-    int uniquePathsMemoization(int m, int n, int cntX, int cntY, int **dp) {
-        if (cntX > m)
-            return 0;
-        if (cntY > n)
-            return 0;
-        if (cntX == m && cntY == n)
-            return 1;
-        if (dp[cntX][cntY] != -1)
-            return dp[cntX][cntY];
-        int x = uniquePathsMemoization(m, n, cntX + 1, cntY, dp);
-        int y = uniquePathsMemoization(m, n, cntX, cntY + 1, dp);
-        dp[cntX][cntY] = x + y;
-        return x + y;
-    }
-
-    int uniquePathsMemoizationUtil(int m, int n) {
-        int **dp = new int *[m + 1];
-        for (int i = 0; i <= m; i++) {
-            dp[i] = new int[n + 1];
+    int uniquePathsDynamicProgramming(int m, int n) {
+        int **dp = new int *[m];
+        for (int i = 0; i < m; i++) {
+            dp[i] = new int[n];
         }
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                dp[i][j] = -1;
+
+        for (int i = 0; i < m; i++) {
+            dp[i][n - 1] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[m - 1][i] = 1;
+        }
+        dp[m - 1][n - 1] = 1;
+
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
             }
         }
-        return uniquePathsMemoization(m - 1, n - 1, 0, 0, dp);
+        return dp[0][0];
     }
     int uniquePaths(int m, int n) {
-        return uniquePathsMemoizationUtil(m,n);   
+        return uniquePathsDynamicProgramming(m,n);
     }
 };
