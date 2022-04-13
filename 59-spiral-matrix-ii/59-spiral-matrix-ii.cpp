a63033a4615bda76fc4dac3684d8b3a7f1e0ef88
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-04-14 00:44:59 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (18.13%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -1,29 +1,87 @@
 class Solution {
 public:
+    void generateMatrixLayerWise(vector<vector<int>> &ans, int &count, int startX, int startY, int xNegativeLim,
+                                 int xPositiveLim, int yNegativeLim, int yPositiveLim) {
+        int cntX = startX;
+        int cntY = startY;
+        char cntDir = 'R';
+
+        while (true) {
+            if (cntX == startX && cntY == startY && cntDir == 'U')
+                break;
+            if (cntDir == 'R') {
+                if (cntY < yPositiveLim) {
+                    ans[cntX][cntY] = count;
+                    count++;
+                    cntY++;
+                } else {
+                    cntDir = 'D';
+                    cntX++;
+                    cntY--;
+                }
+            } else if (cntDir == 'D') {
+                if (cntX < xPositiveLim) {
+                    ans[cntX][cntY] = count;
+                    count++;
+                    cntX++;
+                } else {
+                    cntDir = 'L';
+                    cntY--;
+                    cntX--;
+                }
+            } else if (cntDir == 'L') {
+                if (cntY >= yNegativeLim) {
+                    ans[cntX][cntY] = count;
+                    count++;
+                    cntY--;
+                } else {
+                    cntDir = 'U';
+                    cntX--;
+                    cntY++;
+                }
+            } else {
+                if (cntX >= xNegativeLim) {
+                    ans[cntX][cntY] = count;
+                    count++;
+                    cntX--;
+                } else {
+                    cntX++;
+                    break;
+                }
+            }
+        }
+    }
 
     vector<vector<int>> generateMatrix(int n) {
-
-        vector<vector<int>> result (n, vector<int>(n));
-        int cnt = 1;
-        for (int layer = 0; layer < (n + 1) / 2; layer++) {
-            // direction 1 - traverse from left to right
-            for (int ptr = layer; ptr < n - layer; ptr++) {
-                result[layer][ptr] = cnt++;
-            }
-            // direction 2 - traverse from top to bottom
-            for (int ptr = layer + 1; ptr < n - layer; ptr++) {
-                result[ptr][n - layer - 1] = cnt++;
-            }
-            // direction 3 - traverse from right to left
-            for (int ptr = n - layer - 2; ptr >= layer; ptr--) {
-                result[n - layer - 1][ptr] = cnt++;
-            }
-            // direction 4 - traverse from bottom to top
-            for (int ptr = n - layer - 2; ptr > layer; ptr--) {
-                result[ptr][layer] = cnt++;
+        vector<vector<int>> ans;
+        for (int i = 0; i < n; i++) {
+            vector<int> tmp;
+            ans.push_back(tmp);
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans.at(i).push_back(-1);
             }
         }
 
-        return result;
+        int startX = 0;
+        int startY = 0;
+        int count = 1;
+        int xNegativeLim = 0;
+        int xPositiveLim = n;
+        int yNegativeLim = 0;
+        int yPositiveLim = n;
+
+        int layers = int(n / 2)+1;
+        for (int i = 0; i < layers; i++) {
+            generateMatrixLayerWise(ans, count, startX, startY, xNegativeLim, xPositiveLim, yNegativeLim, yPositiveLim);
+            startX++;
+            startY++;
+            xNegativeLim++;
+            xPositiveLim--;
+            yNegativeLim++;
+            yPositiveLim--;
+        }
+        return ans;
     }
 };
