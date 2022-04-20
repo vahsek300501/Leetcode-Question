e064f1df0afb0feac5e1b0f6ad9fa57880d75268
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-04-20 15:34:38 +0530

    Time: 23 ms (96.06%), Space: 24.4 MB (10.53%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
@@ -43,3 +43,9 @@ public:
         return false;
     }
 };
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
