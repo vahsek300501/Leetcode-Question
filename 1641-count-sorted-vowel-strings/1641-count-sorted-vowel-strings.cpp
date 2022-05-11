e44b222d749f3f63ec2dda79792add1ec6b784f3
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-05-11 10:21:12 +0530

    Time: 5 ms (24.57%), Space: 6.1 MB (34.92%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
-    void countVowelUtil(int n, int cntLen, int start, int &count) {
+    int countVowelUtil(int n, int cntLen, int start, int &count,int ** dp) {
         if (cntLen == n) {
-            count++;
-            return;
+            return 1;
         }
+        if(dp[cntLen][start] != -1)
+            return dp[cntLen][start];
         if (cntLen > n)
-            return;
-
+            return 0;
+        int tmpCount = 0;
         for (int i = start; i < 5; i++) {
-            countVowelUtil(n, cntLen + 1, i, count);
+            tmpCount += countVowelUtil(n, cntLen + 1, i, count,dp);
         }
+        dp[cntLen][start] = tmpCount;
+        return tmpCount;
     }
 
     int countVowelStrings(int n) {
         int count = 0;
-        countVowelUtil(n, 0, 0, count);
-        return count;
+        int ** dp = new int*[n+1];
+        for(int i=0;i<=n;i++) {
+            dp[i] = new int[6];
+        }
+        for(int i =0;i<=n;i++) {
+            for(int j =0;j<6;j++) {
+                dp[i][j] = -1;
+            }
+        }
+        int ans = countVowelUtil(n, 0, 0, count,dp);
+        return ans;
     }
 };
