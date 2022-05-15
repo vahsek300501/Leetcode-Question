39a0d82685e9a630af8fa87d0bcbcad60c92b787
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-05-15 17:50:42 +0530

    Time: 824 ms (17.86%), Space: 193.8 MB (5.01%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int minReorder(int n, vector<vector<int>> &connections) {
+        vector<vector<int>> graph;
+        vector<vector<int>> finalGraph;
+        unordered_map<int, unordered_map<int, int>> initialEdges;
+        vector<bool> visited(n, false);
+
+        for (int i = 0; i < n; i++) {
+            vector<int> tmp;
+            graph.push_back(tmp);
+        }
+        for (vector<int> edge:connections) {
+            graph.at(edge.at(0)).push_back(edge.at(1));
+            graph.at(edge.at(1)).push_back(edge.at(0));
+            initialEdges[edge.at(0)][edge.at(1)] = 1;
+        }
+
+        int count = 0;
+        queue<int> traversalQueue;
+        traversalQueue.push(0);
+        visited[0] = true;
+
+        while (!traversalQueue.empty()) {
+            int top = traversalQueue.front();
+            traversalQueue.pop();
+            vector<int> tmp = graph[top];
+            for (int vertex: tmp) {
+                if (!visited[vertex]) {
+                    traversalQueue.push(vertex);
+                    visited[vertex] = true;
+                    if (initialEdges.find(top) != initialEdges.end() &&
+                        initialEdges[top].find(vertex) != initialEdges[top].end())
+                        count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
