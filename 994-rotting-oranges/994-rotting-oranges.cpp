dc10487db9e41662bc6f641b8d4081881d454aae
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-05-26 22:15:24 +0530

    Time: 8 ms (68.74%), Space: 13.4 MB (21.56%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,71 +1,56 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>> &grid) {
-        queue<pair<int, int>> currentOranges;
-        int n = grid.size();
-        int m = grid.at(0).size();
-        int totalFreshOranges = 0;
-        int totalOranges = 0;
+        int n = (int) grid.size();
+        int m = (int) grid.at(0).size();
+        vector<vector<int>> visited(n, vector<int>(m, -1));
+        queue<pair<int, int>> traversalQueue;
+        bool isAllZero = true;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid.at(i).at(j) == 1) {
-                    totalFreshOranges++;
-                    totalOranges++;
+                if (grid[i][j] == 2) {
+                    visited[i][j] = 0;
+                    traversalQueue.push(make_pair(i, j));
                 }
-                if (grid.at(i).at(j) == 2) {
-                    currentOranges.push(make_pair(i, j));
-                    totalOranges++;
-                }
+                if(grid[i][j] != 0)
+                    isAllZero = false;
             }
         }
-        currentOranges.push(make_pair(-1, -1));
-        int cntTime = 0;
-        while (true) {
-            pair<int, int> tmp = currentOranges.front();
-            currentOranges.pop();
-            if (currentOranges.empty() && tmp.first == -1) {
-                break;
-            }
-            if (tmp.first == -1) {
-                cntTime++;
-                currentOranges.push(tmp);
-                continue;
-            }
-            int cntX = tmp.first;
-            int cntY = tmp.second;
 
-            if (cntX - 1 >= 0) {
-                if (grid.at(cntX - 1).at(cntY) == 1) {
-                    currentOranges.push(make_pair(cntX - 1, cntY));
-                    grid.at(cntX - 1).at(cntY) = 2;
-                    totalFreshOranges--;
-                }
-            }
-            if (cntX + 1 < n) {
-                if (grid.at(cntX + 1).at(cntY) == 1) {
-                    currentOranges.push(make_pair(cntX + 1, cntY));
-                    grid.at(cntX + 1).at(cntY) = 2;
-                    totalFreshOranges--;
-                }
-            }
-            if (cntY - 1 >= 0) {
-                if (grid.at(cntX).at(cntY - 1) == 1) {
-                    currentOranges.push(make_pair(cntX, cntY - 1));
-                    grid.at(cntX).at(cntY - 1) = 2;
-                    totalFreshOranges--;
-                }
-            }
-            if (cntY + 1 < m) {
-                if (grid.at(cntX).at(cntY + 1) == 1) {
-                    currentOranges.push(make_pair(cntX, cntY + 1));
-                    grid.at(cntX).at(cntY + 1) = 2;
-                    totalFreshOranges--;
+        if(isAllZero)
+            return 0;
+        
+
+        vector<int> xPos = {1, -1, 0, 0};
+        vector<int> yPos = {0, 0, 1, -1};
+
+        while (!traversalQueue.empty()) {
+            pair<int, int> top = traversalQueue.front();
+            traversalQueue.pop();
+            int nextTime = visited[top.first][top.second] + 1;
+            for (int i = 0; i < 4; i++) {
+                int newX = top.first + xPos[i];
+                int newY = top.second + yPos[i];
+                if (newX < 0 || newY < 0 || newX >= n || newY >= m)
+                    continue;
+                if (grid[newX][newY] == 1 && visited[newX][newY] == -1) {
+                    grid[newX][newY] = 2;
+                    visited[newX][newY] = nextTime;
+                    traversalQueue.push(make_pair(newX, newY));
                 }
             }
         }
 
-        if (totalFreshOranges != 0)
-            return -1;
-        return cntTime;
+        int minTime = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1)
+                    return -1;
+                if (visited[i][j] == -1)
+                    continue;
+                minTime = max(minTime, visited[i][j]);
+            }
+        }
+        return minTime;
     }
 };
