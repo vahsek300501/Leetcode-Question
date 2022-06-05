dbdcbde5ceb34e7fa3943b57687afa822b753632
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-06-06 01:03:35 +0530

    Time: 102 ms (72.16%), Space: 21.2 MB (40.55%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -9,16 +9,23 @@ public:
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < nums.size(); i++) {
+            if (i != 0 && nums[i] == nums[i - 1])
+                continue;
             int firstNum = nums[i];
             int startPtr = i + 1;
             int endPtr = (int) nums.size() - 1;
             while (startPtr < endPtr) {
+                if (startPtr != i + 1 && nums[startPtr] == nums[startPtr - 1]) {
+                    startPtr++;
+                    continue;
+                }
                 int secondNum = nums[startPtr];
                 int thirdNum = nums[endPtr];
                 int sum = firstNum + secondNum + thirdNum;
                 if (sum == 0) {
                     vector<int> tmp = {firstNum, secondNum, thirdNum};
-                    ansSet.insert(tmp);
+//                    ansSet.insert(tmp);
+                    ans.push_back(tmp);
                     startPtr++;
                     endPtr--;
                 } else if (sum < 0) {
@@ -29,11 +36,11 @@ public:
             }
         }
 
-        auto itr = ansSet.begin();
-        while (itr != ansSet.end()) {
-            ans.push_back(*itr);
-            itr++;
-        }
+//        auto itr = ansSet.begin();
+//        while (itr != ansSet.end()) {
+//            ans.push_back(*itr);
+//            itr++;
+//        }
         return ans;
     }
 };
