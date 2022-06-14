7c0d30d9e3656ce85888addbcbfc4e2c99f04035
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-06-15 00:30:08 +0530

    Time: 15 ms (14.51%), Space: 20.2 MB (7.53%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    void generateAllParenthesisUtil(int n, int cntOpen, int cntClose, string braces,        vector<string> &ans) {
-        if (cntOpen == n && cntClose == n) {
-            ans.push_back(braces);
+    void
+    generateParenthesisUtil(int n, int cntOpeningBrace, int cntClosingBrace, vector<string> &ans, string cntString) {
+        if (cntOpeningBrace == n && cntClosingBrace == n) {
+            ans.push_back(cntString);
             return;
         }
-        if (cntOpen < n) {
-            generateAllParenthesisUtil(n, cntOpen + 1, cntClose, braces + '(', ans);
+        if (cntOpeningBrace <= n) {
+            generateParenthesisUtil(n, cntOpeningBrace + 1, cntClosingBrace, ans, cntString + '(');
         }
-        if (cntClose < n && cntClose < cntOpen) {
-            generateAllParenthesisUtil(n, cntOpen, cntClose + 1, braces + ')', ans);
+        if (cntClosingBrace <= n && cntClosingBrace < cntOpeningBrace) {
+            generateParenthesisUtil(n, cntOpeningBrace, cntClosingBrace+1, ans, cntString + ')');
         }
     }
 
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        generateAllParenthesisUtil(n, 0, 0, "", ans);
+        generateParenthesisUtil(n, 0, 0, ans, "");
         return ans;
     }
 };
