db13caddc158fbceae2c9fdc57b283e22373fe1b
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-06-16 22:54:48 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (47.01%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    int robMemoization(vector<int> &nums, int cntIndex, vector<int> &dp) {
-        if (cntIndex >= nums.size())
-            return 0;
-        if (dp[cntIndex] != -1)
-            return dp[cntIndex];
-        int first = nums[cntIndex] + robMemoization(nums, cntIndex + 2, dp);
-        int second = robMemoization(nums, cntIndex + 1, dp);
-        dp[cntIndex] = max(first, second);
-        return max(first, second);
+    int robDynamicProgramming(vector<int> &nums) {
+        vector<int> dp(nums.size(), 0);
+        if (nums.size() == 1)
+            return nums[0];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+//          Exclusion
+            int exclusion = dp[i - 1];
+//          Inclusion
+            int inclusion = nums[i];
+            if (i - 2 >= 0) {
+                inclusion += dp[i - 2];
+            }
+            dp[i] = max(inclusion, exclusion);
+        }
+        return dp[nums.size() - 1];
+
     }
 
+
     int rob(vector<int> &nums) {
-        vector<int> dp(nums.size() + 1, -1);
-        return robMemoization(nums, 0, dp);
+        return robDynamicProgramming(nums);
     }
 };
