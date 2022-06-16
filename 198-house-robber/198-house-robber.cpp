f9059359f9b53444836dd1785824348f4d5131df
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-06-16 22:56:42 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (25.11%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -5,15 +5,11 @@ public:
         if (nums.size() == 1)
             return nums[0];
         dp[0] = nums[0];
-        for (int i = 1; i < nums.size(); i++) {
-//          Exclusion
-            int exclusion = dp[i - 1];
-//          Inclusion
-            int inclusion = nums[i];
-            if (i - 2 >= 0) {
-                inclusion += dp[i - 2];
-            }
-            dp[i] = max(inclusion, exclusion);
+        dp[1] = max(nums[1],dp[0]);
+        if(nums.size() == 2)
+            return max(dp[0],dp[1]);
+        for (int i = 2; i < nums.size(); i++) {
+            dp[i] = max(dp[i-1],dp[i-2]+nums[i]);
         }
         return dp[nums.size() - 1];
 
