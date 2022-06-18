8807299c982cf35be77714396e5baf0b0c76175f
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-06-18 19:13:02 +0530

    Time: 8 ms (66.00%), Space: 9.1 MB (21.36%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int
-    minimumTotalMemoization(vector<vector<int>> &triangle, int cntX, int cntY, int n, int m, vector<vector<int>> &dp) {
-        if (cntX < 0 || cntY < 0 || cntX >= n || cntY >= m)
-            return 0;
-        if (dp[cntX][cntY] != -1)
-            return dp[cntX][cntY];
-        int first = minimumTotalMemoization(triangle, cntX + 1, cntY, n, cntX + 2, dp) + triangle[cntX][cntY];
-        int second = minimumTotalMemoization(triangle, cntX + 1, cntY + 1, n, cntX + 2, dp) + triangle[cntX][cntY];
-        dp[cntX][cntY] = min(first, second);
-        return min(first, second);
+    int minimumTotalDynamicProgramming(vector<vector<int>> &triangle) {
+        int n = (int) triangle.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            dp[n - 1][i] = triangle[n - 1][i];
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < i + 1; j++) {
+                dp[i][j] = min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j];
+            }
+        }
+        return dp[0][0];
     }
 
     int minimumTotal(vector<vector<int>> &triangle) {
-        int n = (int) triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return minimumTotalMemoization(triangle, 0, 0, n, 1, dp);
+        return minimumTotalDynamicProgramming(triangle);
     }
 };
