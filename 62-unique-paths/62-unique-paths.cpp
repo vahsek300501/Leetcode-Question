004756a072db02933a77fb1488649912129f3308
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-06-18 16:29:56 +0530

    Time: 8 ms (6.27%), Space: 6.5 MB (25.32%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    int uniquePathsDynamicProgramming(int m, int n) {
-        int **dp = new int *[m];
-        for (int i = 0; i < m; i++) {
-            dp[i] = new int[n];
-        }
-
-        for (int i = 0; i < m; i++) {
-            dp[i][n - 1] = 1;
-        }
-        for (int i = 0; i < n; i++) {
-            dp[m - 1][i] = 1;
-        }
-        dp[m - 1][n - 1] = 1;
-
-        for (int i = m - 2; i >= 0; i--) {
-            for (int j = n - 2; j >= 0; j--) {
-                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
-            }
-        }
-        return dp[0][0];
+    int uniquePathsMemoization(int m, int n, int cntX, int cntY, vector<vector<int>> &dp) {
+        if (cntX < 0 || cntY < 0 || cntX >= m || cntY >= n)
+            return 0;
+        if (cntX == m - 1 && cntY == n - 1)
+            return 1;
+        if (dp[cntX][cntY] != -1)
+            return dp[cntX][cntY];
+        int ans = uniquePathsMemoization(m, n, cntX + 1, cntY, dp) + uniquePathsMemoization(m, n, cntX, cntY + 1, dp);
+        dp[cntX][cntY] = ans;
+        return ans;
     }
+
+    int uniquePathMemoizationUtil(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return uniquePathsMemoization(m, n, 0, 0, dp);
+    }
+
     int uniquePaths(int m, int n) {
-        return uniquePathsDynamicProgramming(m,n);
+        return uniquePathMemoizationUtil(m, n);
     }
 };
