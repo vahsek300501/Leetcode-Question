55a1dca10964d15112143f3949b42c7a23fa5199
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-06-23 18:50:09 +0530

    Time: 23 ms (13.21%), Space: 14.2 MB (37.60%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int newColor) {
+    void
+    floodFillUtil(vector<vector<int>> &image, int n, int m, int cntX, int cntY, int color,
+                  vector<vector<bool>> &visited,
+                  int prevColor) {
+        if (cntX < 0 || cntY < 0 || cntX >= n || cntY >= m)
+            return;
+        if (visited[cntX][cntY])
+            return;
+        if (image[cntX][cntY] != prevColor)
+            return;
+
+        visited[cntX][cntY] = true;
+        image[cntX][cntY] = color;
+        floodFillUtil(image, n, m, cntX + 1, cntY, color, visited, prevColor);
+        floodFillUtil(image, n, m, cntX - 1, cntY, color, visited, prevColor);
+        floodFillUtil(image, n, m, cntX, cntY + 1, color, visited, prevColor);
+        floodFillUtil(image, n, m, cntX, cntY - 1, color, visited, prevColor);
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int color) {
         int n = (int) image.size();
         int m = (int) image.at(0).size();
-        vector<vector<bool>> visited;
-        for (int i = 0; i < n; i++) {
-            vector<bool> tmp;
-            visited.push_back(tmp);
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                visited[i].push_back(false);
-            }
-        }
-
-        queue<pair<int, int>> traversalQueue;
-        traversalQueue.push(make_pair(sr, sc));
-        visited[sr][sc] = true;
-        vector<int> xPos = {0, 0, 1, -1};
-        vector<int> yPos = {1, -1, 0, 0};
-
-        while (!traversalQueue.empty()) {
-            pair<int, int> top = traversalQueue.front();
-            traversalQueue.pop();
-            int orgVal = image[top.first][top.second];
-            image[top.first][top.second] = newColor;
-            for (int i = 0; i < 4; i++) {
-                int newX = top.first + xPos[i];
-                int newY = top.second + yPos[i];
-                if (newX < 0 || newY < 0 || newX >= n || newY >= m)
-                    continue;
-                if (visited[newX][newY])
-                    continue;
-                if (image[newX][newY] != orgVal)
-                    continue;
-                visited[newX][newY] = true;
-                traversalQueue.push(make_pair(newX, newY));
-            }
-        }
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        floodFillUtil(image, n, m, sr, sc, color, visited, image[sr][sc]);
         return image;
     }
 };
