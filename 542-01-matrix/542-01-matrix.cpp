842053bc35357deb68acc93de068477470f3b1d8
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-06-24 19:48:05 +0530

    Time: 113 ms (52.27%), Space: 31.8 MB (25.30%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -3,35 +3,37 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>> &mat) {
         int n = (int) mat.size();
         int m = (int) mat.at(0).size();
-        vector<vector<int>> visited(n, vector<int>(m, -1));
-        queue<pair<int, int>> traversalQueue;
-
+        queue<pair<int, pair<int, int>>> traversalQueue;
+        vector<vector<int>> distance(n, vector<int>(m, -1));
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (mat[i][j] == 0) {
-                    traversalQueue.push(make_pair(i, j));
-                    visited[i][j] = 0;
+                    distance[i][j] = 0;
+                    traversalQueue.push(make_pair(0, make_pair(i, j)));
                 }
             }
         }
 
         vector<int> xPos = {1, -1, 0, 0};
         vector<int> yPos = {0, 0, 1, -1};
+
         while (!traversalQueue.empty()) {
-            pair<int, int> top = traversalQueue.front();
+            pair<int, pair<int, int>> top = traversalQueue.front();
             traversalQueue.pop();
-            int nextNodeVal = visited[top.first][top.second] + 1;
+
             for (int i = 0; i < 4; i++) {
-                int newX = top.first + xPos[i];
-                int newY = top.second + yPos[i];
+                int newX = top.second.first + xPos[i];
+                int newY = top.second.second + yPos[i];
                 if (newX < 0 || newY < 0 || newX >= n || newY >= m)
                     continue;
-                if (mat[newX][newY] == 1 && visited[newX][newY] == -1) {
-                    visited[newX][newY] = nextNodeVal;
-                    traversalQueue.push(make_pair(newX, newY));
-                }
+                if (distance[newX][newY] != -1)
+                    continue;
+                if (mat[newX][newY] == 0)
+                    continue;
+                distance[newX][newY] = top.first + 1;
+                traversalQueue.push(make_pair(top.first + 1, make_pair(newX, newY)));
             }
         }
-        return visited;
+        return distance;
     }
 };
