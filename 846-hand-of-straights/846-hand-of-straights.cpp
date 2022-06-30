6f9e0ef54ee49b882747c614d91a4c51cb629d29
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-06-30 23:42:48 +0530

    Time: 191 ms (10.12%), Space: 28.4 MB (20.98%) - LeetHub

---
+++ 846-hand-of-straights/846-hand-of-straights.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isPossibleDivide(vector<int> &nums, int k) {
+        int n = (int) nums.size();
+        unordered_map<int, int> freqMap;
+        priority_queue<int, vector<int>, greater<>> minHeap;
+        for (int i = 0; i < n; i++) {
+            if (freqMap.find(nums[i]) == freqMap.end()) {
+                freqMap[nums[i]] = 1;
+                minHeap.push(nums[i]);
+            } else {
+                freqMap[nums[i]] += 1;
+            }
+        }
+
+        while (!minHeap.empty()) {
+            int minimum = minHeap.top();
+            if (freqMap[minimum] == 0) {
+                minHeap.pop();
+                continue;
+            }
+            freqMap[minimum]--;
+            int tmp = minimum + 1;
+            for (int i = 0; i < k - 1; i++) {
+                if (freqMap.find(tmp) != freqMap.end() && freqMap[tmp] > 0) {
+                    freqMap[tmp]--;
+                    tmp++;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        return isPossibleDivide(hand,groupSize);   
+    }
+};
