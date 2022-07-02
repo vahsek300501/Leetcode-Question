c55aa10d19d9e1faf741adf057e372c7c24b8bcd
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-07-02 22:32:25 +0530

    Time: 56 ms (23.74%), Space: 12.3 MB (35.87%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
-    int lcsDynamicProgramming(string str1, string str2) {
-        int **dp = new int *[str1.length() + 1];
-        for (int i = 0; i <= str1.length(); i++) {
-            dp[i] = new int[str2.length() + 1];
-        }
-        for (int i = 0; i <= str1.length(); i++) {
-            dp[i][0] = 0;
-        }
-        for (int i = 0; i <= str2.length(); i++) {
-            dp[0][i] = 0;
-        }
-
-        for (int i = 1; i <= str1.length(); i++) {
-            for (int j = 1; j <= str2.length(); j++) {
-                if (str1[i - 1] == str2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+    static int longestCommonSubsequenceDynamicProgramming(string &s1, string &s2) {
+        vector<vector<int>> dp(s1.size() + 1, vector<int>(s2.size() + 1, 0));
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        return dp[str1.length()][str2.length()];
+        return dp[(int) s1.size()][(int) s2.size()];
     }
-    
+
+    static int longestCommonSubsequence(string text1, string text2) {
+        return longestCommonSubsequenceDynamicProgramming(text1, text2);
+    }
     int minDistance(string word1, string word2) {
-        int lcs = lcsDynamicProgramming(word1,word2);   
-        int count = 0;
-        if(lcs < word1.length()) {
-            count += (word1.length() - lcs);
-        }
-        if(lcs < word2.length()) {
-            count += (word2.length() - lcs);
-        }
-        return count;
+        int n = (int) word1.size();
+        int m = (int) word2.size();
+        int lcs = longestCommonSubsequenceDynamicProgramming(word1, word2);
+        return m + n - (2 * lcs);
     }
 };
