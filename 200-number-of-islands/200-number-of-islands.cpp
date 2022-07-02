ff03321263af0fc9c3d4d14aa86a0ebb25a4d316
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-07-03 00:32:42 +0530

    Time: 69 ms (26.39%), Space: 12.6 MB (37.39%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
-    void dfsIslands(vector<vector<char>> &grid, vector<vector<bool>> &visited, int cntX, int cntY, int n, int m) {
+    void numIslandUtil(vector<vector<char>> &grid, vector<vector<bool>> &visited, int n, int m, int cntX, int cntY) {
         if (cntX < 0 || cntY < 0 || cntX >= n || cntY >= m)
             return;
-        if (visited[cntX][cntY])
+        if(visited[cntX][cntY])
             return;
         if (grid[cntX][cntY] == '0')
             return;
         visited[cntX][cntY] = true;
-        dfsIslands(grid, visited, cntX + 1, cntY, n, m);
-        dfsIslands(grid, visited, cntX - 1, cntY, n, m);
-        dfsIslands(grid, visited, cntX, cntY + 1, n, m);
-        dfsIslands(grid, visited, cntX, cntY - 1, n, m);
+        numIslandUtil(grid, visited, n, m, cntX + 1, cntY);
+        numIslandUtil(grid, visited, n, m, cntX - 1, cntY);
+        numIslandUtil(grid, visited, n, m, cntX, cntY + 1);
+        numIslandUtil(grid, visited, n, m, cntX, cntY - 1);
     }
 
     int numIslands(vector<vector<char>> &grid) {
-        vector<vector<bool>> visited;
         int n = (int) grid.size();
         int m = (int) grid.at(0).size();
-        for (int i = 0; i < n; i++) {
-            vector<bool> tmp;
-            visited.push_back(tmp);
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                visited[i].push_back(false);
-            }
-        }
-
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
         int count = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == '1' && !visited[i][j]) {
                     count++;
-                    dfsIslands(grid, visited, i, j, n, m);
+                    numIslandUtil(grid, visited, n, m, i, j);
                 }
             }
         }
