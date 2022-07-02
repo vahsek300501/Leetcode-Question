b7c5d230e91f6bb69cac2e228ee041466d2b95c9
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-07-02 19:44:46 +0530

    Time: 414 ms (7.62%), Space: 73 MB (44.16%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
-    int lcsDynamicProgramming(string str1, string str2) {
-        int **dp = new int *[str1.length() + 1];
-        for (int i = 0; i <= str1.length(); i++) {
-            dp[i] = new int[str2.length() + 1];
-        }
-        for (int i = 0; i <= str1.length(); i++) {
-            dp[i][0] = 0;
-        }
-        for (int i = 0; i <= str2.length(); i++) {
-            dp[0][i] = 0;
-        }
-
-        for (int i = 1; i <= str1.length(); i++) {
-            for (int j = 1; j <= str2.length(); j++) {
-                if (str1[i - 1] == str2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+    static int longestCommonSubsequenceDynamicProgramming(string &s1, string &s2) {
+        vector<vector<int>> dp(s1.size() + 1, vector<int>(s2.size() + 1, 0));
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        return dp[str1.length()][str2.length()];
+        return dp[(int) s1.size()][(int) s2.size()];
     }
-    int longestPalindromicSubsequenceDynamicProgramming(string s) {
-        string tmpStr2 = "";
-        for (int i = s.length() - 1; i >= 0; i--) {
-            tmpStr2 += s.at(i);
-        }
-        return lcsDynamicProgramming(s, tmpStr2);
-    }
-
+    
     int longestPalindromeSubseq(string s) {
-        return longestPalindromicSubsequenceDynamicProgramming(s);
+        string rev(s);
+        reverse(rev.begin(), rev.end());
+        return longestCommonSubsequenceDynamicProgramming(s, rev);
     }
 };
