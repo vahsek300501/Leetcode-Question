acda2339895a9918d5bb475ad1e2dd207e0a7cb6
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-07-04 18:15:20 +0530

    Time: 226 ms (29.07%), Space: 93.4 MB (52.18%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -5,12 +5,9 @@ public:
         int mini = prices[0];
         int maxProfit = 0;
         for (int i = 0; i < n; i++) {
-            if (prices[i] <= mini) {
-                mini = prices[i];
-                continue;
-            }
             int tmpProfit = prices[i] - mini;
             maxProfit = max(maxProfit, tmpProfit);
+            mini = min(mini, prices[i]);
         }
         return maxProfit;
     }
