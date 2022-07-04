f4044a59f14f8f02a24a4d3bd3640a85cb3992c8
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-07-04 23:07:25 +0530

    Time: 1279 ms (10.90%), Space: 207.1 MB (28.56%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    int maxProfit3Recursion(vector<int> &prices, int n, int cntIndex, int canBuy, int cntTransaction,
-                            vector<vector<vector<int>>> &dp) {
-        if (cntIndex >= n)
-            return 0;
-        if (cntTransaction >= 2)
-            return 0;
-        if (dp[cntIndex][canBuy][cntTransaction] != -1)
-            return dp[cntIndex][canBuy][cntTransaction];
-        int profit;
-        if (canBuy == 1) {
-            profit = max(-prices[cntIndex] + maxProfit3Recursion(prices, n, cntIndex + 1, 0, cntTransaction, dp),
-                         maxProfit3Recursion(prices, n, cntIndex + 1, 1, cntTransaction, dp));
-        } else {
-            profit = max(prices[cntIndex] + maxProfit3Recursion(prices, n, cntIndex + 1, 1, cntTransaction + 1, dp),
-                         maxProfit3Recursion(prices, n, cntIndex+1, 0, cntTransaction, dp));
+    int maxProfit3DynamicProgramming(vector<int> &prices) {
+        int n = (int) prices.size();
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+
+        for (int cntIndex = n - 1; cntIndex >= 0; cntIndex--) {
+            for (int canBuy = 0; canBuy < 2; canBuy++) {
+                for (int cntTransaction = 0; cntTransaction < 2; cntTransaction++) {
+                    int profit;
+                    if (canBuy) {
+                        profit = max(-prices[cntIndex] + dp[cntIndex + 1][0][cntTransaction],
+                                     dp[cntIndex + 1][1][cntTransaction]);
+                    } else {
+                        profit = max(prices[cntIndex] + dp[cntIndex + 1][1][cntTransaction + 1],
+                                     dp[cntIndex + 1][0][cntTransaction]);
+                    }
+                    dp[cntIndex][canBuy][cntTransaction] = profit;
+                }
+            }
         }
-        return dp[cntIndex][canBuy][cntTransaction] = profit;
+        return dp[0][1][0];
     }
 
-    int maxProfit3(vector<int> &prices) {
-        int n = (int) prices.size();
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, -1)));
-        return maxProfit3Recursion(prices, n, 0, 1, 0, dp);
-    }
     int maxProfit(vector<int>& prices) {
-        return maxProfit3(prices);   
+        return maxProfit3DynamicProgramming(prices);        
     }
 };
