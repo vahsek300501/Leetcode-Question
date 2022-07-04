cad2b64275df557f8535f12de55de7df5597a1ed
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-07-05 00:56:25 +0530

    Time: 292 ms (29.04%), Space: 90.8 MB (38.48%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    int maxProfit6DynamicProgramming(vector<int> &prices, int fees) {
+        int n = (int) prices.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+        for (int cntIndex = n - 1; cntIndex >= 0; cntIndex--) {
+            for (int canBuy = 0; canBuy < 2; canBuy++) {
+                int profit;
+                if (canBuy == 1) {
+                    profit = max(-prices[cntIndex] + dp[cntIndex + 1][0], dp[cntIndex + 1][1]);
+                } else {
+                    profit = max(prices[cntIndex] - fees + dp[cntIndex + 1][1], dp[cntIndex + 1][0]);
+                }
+                dp[cntIndex][canBuy] = profit;
+            }
+        }
+        return dp[0][1];
+    }
     int maxProfit6Recursive(vector<int> &prices, int n, int cntIndex, int canBuy, int fees, vector<vector<int>> &dp) {
         if (cntIndex >= n)
             return 0;
@@ -25,6 +42,6 @@ public:
         return maxProfit6Recursive(prices, n, 0, 1, fee, dp);
     }
     int maxProfit(vector<int>& prices, int fee) {
-        return maxProfit6(prices,fee);   
+        return maxProfit6DynamicProgramming(prices,fee);
     }
 };
