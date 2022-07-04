ec11d45349d53ceb607aeda1c6b922a4986bb897
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-07-05 00:13:33 +0530

    Time: 17 ms (53.61%), Space: 13.1 MB (30.60%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    int maxProfit4DynamicProgramming(int k ,vector<int> &prices) {
+        int n = (int) prices.size();
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+
+        for (int cntIndex = n - 1; cntIndex >= 0; cntIndex--) {
+            for (int canBuy = 0; canBuy < 2; canBuy++) {
+                for (int cntTransaction = 0; cntTransaction < k; cntTransaction++) {
+                    int profit;
+                    if (canBuy) {
+                        profit = max(-prices[cntIndex] + dp[cntIndex + 1][0][cntTransaction],
+                                     dp[cntIndex + 1][1][cntTransaction]);
+                    } else {
+                        profit = max(prices[cntIndex] + dp[cntIndex + 1][1][cntTransaction + 1],
+                                     dp[cntIndex + 1][0][cntTransaction]);
+                    }
+                    dp[cntIndex][canBuy][cntTransaction] = profit;
+                }
+            }
+        }
+        return dp[0][1][0];
+    }
     int maxProfit4Recursion(vector<int> &prices, int n, int cntIndex, int canBuy, int cntTransaction,
                             vector<vector<vector<int>>> &dp) {
         if (cntIndex >= n)
@@ -26,6 +47,6 @@ public:
         return maxProfit4Recursion(prices, n, 0, 1, k, dp);
     }
     int maxProfit(int k, vector<int>& prices) {
-        return maxProfit4(k,prices);   
+        return maxProfit4DynamicProgramming(k,prices);   
     }
 };
