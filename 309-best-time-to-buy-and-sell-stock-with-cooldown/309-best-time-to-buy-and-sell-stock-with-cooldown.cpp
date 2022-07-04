239a29de6cb04acbd309de571411bff849fc49cb
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-07-05 00:40:22 +0530

    Time: 11 ms (32.61%), Space: 11.9 MB (43.12%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+    static int maxProfit5DynamicProgramming(vector<int> &prices) {
+        int n = (int) prices.size();
+        vector<vector<int>> dp(n + 2, vector<int>(2, 0));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy < 2; buy++) {
+                int profit;
+                if (buy == 0) {
+                    profit = max(-prices[i] + dp[i + 1][1], dp[i + 1][0]);
+                } else {
+                    profit = max(prices[i] + dp[i + 2][0], dp[i + 1][1]);
+                }
+                dp[i][buy] = profit;
+            }
+        }
+        return dp[0][0];
+    }
     int maxProfit5Recursion(vector<int> &prices, int n, int cntIndex, int canBuy, vector<vector<int>> &dp) {
         if (cntIndex >= n)
             return 0;
@@ -24,6 +40,6 @@ public:
         return maxProfit5Recursion(prices, n, 0, 0, dp);
     }
     int maxProfit(vector<int>& prices) {
-        return maxProfit5(prices);   
+        return maxProfit5DynamicProgramming(prices);
     }
 };
