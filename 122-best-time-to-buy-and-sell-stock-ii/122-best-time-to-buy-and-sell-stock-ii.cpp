8cb713b19832e5427778ad723f327ca67180a9d9
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-07-04 18:57:35 +0530

    Time: 29 ms (5.61%), Space: 15.2 MB (13.97%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    int maxProfit2Recursion(vector<int> &prices, int n, int cntIndex, int canBuy, vector<vector<int>> &dp) {
-        if (cntIndex >= n)
-            return 0;
-        if (dp[cntIndex][canBuy] != -1)
-            return dp[cntIndex][canBuy];
-
-        int profit;
-        if (canBuy == 0) {
-            profit = max(-prices[cntIndex] + maxProfit2Recursion(prices, n, cntIndex + 1, 1, dp),
-                         maxProfit2Recursion(prices, n, cntIndex + 1, 0, dp));
-        } else {
-            profit = max(prices[cntIndex] + maxProfit2Recursion(prices, n, cntIndex + 1, 0, dp),
-                         maxProfit2Recursion(prices, n, cntIndex + 1, 1, dp));
-        }
-        dp[cntIndex][canBuy] = profit;
-        return profit;
-    }
-
-    int maxProfit2(vector<int> &prices) {
+    int maxProfit2DynamicProgramming(vector<int> &prices) {
         int n = (int) prices.size();
-        vector<vector<int>> dp(n + 1, vector<int>(2, -1));
-        return maxProfit2Recursion(prices, n, 0, 0, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy < 2; buy++) {
+                int profit;
+                if (buy) {
+                    profit = max(-prices[i] + dp[i + 1][0], dp[i + 1][1]);
+                } else {
+                    profit = max(prices[i] + dp[i + 1][1], dp[i + 1][0]);
+                }
+                dp[i][buy] = profit;
+            }
+        }
+        return dp[0][1];
     }
     int maxProfit(vector<int>& prices) {
-        return maxProfit2(prices);   
+        return maxProfit2DynamicProgramming(prices);
     }
 };
