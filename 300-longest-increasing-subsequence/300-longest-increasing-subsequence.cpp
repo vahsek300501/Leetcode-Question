40f71855e0e097f87ca3b27bd89756e2badec855
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-07-06 13:58:13 +0530

    Time: 12 ms (86.25%), Space: 10.4 MB (66.79%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int> &nums) {
+    int lengthOfLISBinarySearch(vector<int> &nums) {
         int n = (int) nums.size();
-        vector<int> dp(n, 1);
+        vector<int> cntSequence;
+        cntSequence.push_back(nums[0]);
         for (int i = 1; i < n; i++) {
             int cntElement = nums[i];
-            for (int j = i - 1; j >= 0; j--) {
-                if (nums[j] < cntElement && dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
-                }
-            }
+            int index = lower_bound(cntSequence.begin(), cntSequence.end(), cntElement) - cntSequence.begin();
+            if (index >= (int) cntSequence.size())
+                cntSequence.push_back(cntElement);
+            else
+                cntSequence[index] = cntElement;
         }
-        int maxi = 1;
-        for (int i = 0; i < n; i++) {
-            maxi = max(maxi, dp[i]);
-        }
-        return maxi;
+        return (int) cntSequence.size();
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        return lengthOfLISBinarySearch(nums);   
     }
 };
