109aa20fe49cd7c1146cd3f1c3a61c6861ed5b19
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-08-10 01:32:06 +0530

    Time: 54 ms (95.69%), Space: 9.2 MB (67.38%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -1,35 +1,32 @@
 #define MOD 1000000007
 class Solution {
 public:
-    int
-    numFactorsBinaryTreeRecursion(vector<int> &arr, int cntNum, unordered_map<int, int> &countMap) {
-        if (countMap[cntNum] != -1)
-            return countMap[cntNum];
-        int ans = 1;
-        for (int val : arr) {
-            if (cntNum % val == 0 && countMap.find(cntNum / val) != countMap.end()) {
-                int first = numFactorsBinaryTreeRecursion(arr, val, countMap);
-                int second = numFactorsBinaryTreeRecursion(arr, cntNum / val, countMap);
-                long long int tmp = (((long long int)first % MOD) * ((long long int)second % MOD)) % MOD;
-                ans = ((tmp % MOD) + (ans % MOD)) % MOD;
+    int numFactorBinaryTreeDynamicProgramming(vector<int> &arr) {
+        int n = (int) arr.size();
+        sort(arr.begin(), arr.end());
+        unordered_map<long long int, long long int> countMap;
+        for (int i = 0; i < n; i++) {
+            countMap[arr[i]] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            int cntNum = arr[i];
+            for (int j = 0; j < i; j++) {
+                if (cntNum % arr[j] == 0 && countMap.find(cntNum / arr[j]) != countMap.end()) {
+                    long long int first = countMap[arr[j]];
+                    long long int second = countMap[cntNum / arr[j]];
+                    long long int tmp = (((long long int) first % MOD) * ((long long int) second % MOD)) % MOD;
+                    countMap[cntNum] = ((countMap[cntNum] % MOD) + (tmp % MOD)) % MOD;
+                }
             }
         }
-        countMap[cntNum] = ans;
-        return ans;
+        long long int ans = 0;
+        for (int tmpNode : arr) {
+            ans = ((ans % MOD) + (countMap[tmpNode] % MOD)) % MOD;
+        }
+        return (int) ans;
     }
 
     int numFactoredBinaryTrees(vector<int> &arr) {
-        sort(arr.begin(), arr.end());
-        int n = (int) arr.size();
-        unordered_map<int, int> countMap;
-        for (int i = 0; i < n; i++) {
-            countMap[arr[i]] = -1;
-        }
-        int finalAns = 0;
-        for (int val:arr) {
-            int tmp = numFactorsBinaryTreeRecursion(arr, val, countMap);
-            finalAns = ((finalAns % MOD) + (tmp % MOD)) % MOD;
-        }
-        return finalAns;
+        return numFactorBinaryTreeDynamicProgramming(arr);
     }
 };
