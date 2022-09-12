7ed253ac047bbe9f8d22f28e0d12f29f2e97b7dc
Keshav <58917332+vahsek300501@users.noreply.github.com>
2022-09-12 22:50:24 +0530

    Time: 8 ms (72.03%), Space: 10.5 MB (94.44%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.cpp
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     int bagOfTokensScore(vector<int> &tokens, int power) {
+        if(tokens.size() == 0)
+            return 0;
         sort(tokens.begin(), tokens.end());
-        int first = 0;
-        int last = (int) tokens.size() - 1;
         int cntScore = 0;
+        long long int cntPower = power;
+        int firstPtr = 0;
+        int lastPtr = (int) tokens.size() - 1;
         int maxScore = 0;
-
-        while (first <= last) {
-            if (first == last) {
-                if (power >= tokens[first]) {
+        while (firstPtr <= lastPtr) {
+            if (firstPtr == lastPtr) {
+                if (cntPower >= tokens[firstPtr])
                     cntScore++;
-                    maxScore = max(maxScore, cntScore);
-                    power = power - tokens[first];
-                }
                 break;
             }
-            if (power >= tokens[first]) {
+            if (cntPower >= tokens[firstPtr]) {
+                cntPower = cntPower - tokens[firstPtr];
+                firstPtr++;
                 cntScore++;
                 maxScore = max(maxScore, cntScore);
-                power = power - tokens[first];
-                first++;
-            } else {
-                if (cntScore > 0) {
-                    power += tokens[last];
-                    last--;
-                    cntScore--;
-                } else {
-                    break;
-                }
+                continue;
             }
+            if (cntScore > 0) {
+                cntPower += tokens[lastPtr];
+                lastPtr--;
+                cntScore--;
+                continue;
+            }
+            if (cntScore <= 0 && cntPower < tokens[firstPtr])
+                break;
         }
-        return maxScore;
+        return max(cntScore, maxScore);
     }
 };
